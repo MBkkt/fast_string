217799430044cb80e420fa9db6e282ffdf92df27
MBkkt <MBkkt@users.noreply.github.com>
2021-04-11 17:44:55 +0300

    Add remove and refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub struct FastString(StringInner);
 
 impl FastString {
     #[inline(always)]
-    pub fn new<T>(string: T) -> FastString
+    pub fn new<T>(string: T) -> FastString // TODO empty string and From
         where T: AsRef<str> { // TODO Why?
         FastString(StringInner::new(string.as_ref()))
     }
@@ -32,6 +32,11 @@ impl FastString {
     #[inline(always)]
     pub fn push_str(&mut self, string: &str) {
         self.0.push_str(string);
+    }
+
+    #[inline(always)]
+    pub fn remove(&mut self, idx: usize) -> char {
+        self.0.remove(idx)
     }
 }
 
@@ -133,36 +138,34 @@ const fn max(a: usize, b: usize) -> usize {
     [a, b][(a < b) as usize]
 }
 
-const SMALL_CAP: usize = max(size_of::<Arc<Vec<u8>>>(), 24) - 1;
+const SSO_CAPACITY: usize = max(size_of::<Arc<Vec<u8>>>(), 24) - 1;
 
 #[derive(Clone, Debug)]
 enum StringInner {
-    Small { buf: [u8; SMALL_CAP], len: u8 },
-    Large(Arc<Vec<u8>>),
+    Small { data: [u8; SSO_CAPACITY], len: u8 },
+    Large { data: Arc<Vec<u8>>, len: usize },
 }
 
 impl StringInner {
     fn new(text: &str) -> Self {
-        let len = text.len();
-        return if len <= SMALL_CAP {
-            let mut buf = [0; SMALL_CAP];
-            buf[..len].copy_from_slice(text.as_bytes());
-            StringInner::Small { buf, len: len as u8 }
+        let new_len = text.len();
+        return if new_len <= SSO_CAPACITY {
+            let mut new_data = [0; SSO_CAPACITY];
+            new_data[..new_len].copy_from_slice(text.as_bytes());
+            StringInner::Small { data: new_data, len: new_len as u8 }
         } else {
-            StringInner::Large(Arc::from(Vec::from(text.as_bytes())))
+            StringInner::Large { data: Arc::from(Vec::from(text.as_bytes())), len: new_len }
         };
     }
 
     fn as_str(&self) -> &str {
         match self {
-            StringInner::Small { buf, len } => {
+            StringInner::Small { data, len } => {
                 let len = *len as usize;
-                let buf = &buf[..len];
-                unsafe { from_utf8_unchecked(buf) } // TODO unsafe
+                unsafe { from_utf8_unchecked(&data[..len]) } // TODO unsafe
             }
-            StringInner::Large(data) => {
-                let buf = &data[..data.len()];
-                unsafe { from_utf8_unchecked(buf) } // TODO unsafe
+            StringInner::Large { data, len: _ } => {
+                unsafe { from_utf8_unchecked(data.as_slice()) } // TODO unsafe
             }
         }
     }
@@ -172,34 +175,64 @@ impl StringInner {
         self.push_str(ch.encode_utf8(&mut temp));
     }
 
-    // TODO Is this the optimal way?
     fn push_str(&mut self, string: &str) {
         match self {
-            StringInner::Small { buf, len } => {
+            StringInner::Small { data, len } => {
                 let new_len = *len as usize + string.len();
-                if new_len <= SMALL_CAP {
-                    buf[*len as usize..new_len].copy_from_slice(string.as_bytes());
+                if new_len <= SSO_CAPACITY {
+                    data[*len as usize..new_len].copy_from_slice(string.as_bytes());
                     *len = new_len as u8
                 } else {
                     let mut new_data = Vec::with_capacity(new_len);
-                    new_data.extend_from_slice(&buf[..*len as usize]);
+                    new_data.extend_from_slice(&data[..*len as usize]);
                     new_data.extend_from_slice(string.as_bytes());
-                    *self = StringInner::Large(Arc::from(new_data)); // TODO ugly
+                    *self = StringInner::Large { data: Arc::from(new_data), len: new_len };
                 }
             }
-            StringInner::Large(data) => {
+            StringInner::Large { data, len } => {
                 match Arc::get_mut(data) {
                     Some(old_data) => {
                         old_data.extend_from_slice(string.as_bytes());
+                        *len = old_data.len();
                     }
                     None => {
-                        let mut new_data: Vec<u8> = Vec::with_capacity(data.len() + string.len());
-                        new_data.extend(data.iter());
+                        *len = data.len() + string.len();
+                        let mut new_data = Vec::with_capacity(*len);
+                        new_data.extend_from_slice(data.as_slice());
                         new_data.extend_from_slice(string.as_bytes());
-                        *self = StringInner::Large(Arc::from(new_data));
+                        *data = Arc::from(new_data);
                     }
                 }
             }
         };
+    }
+
+    fn remove(&mut self, idx: usize) -> char {
+        let ch = match self.as_str()[idx..].chars().next() {
+            Some(ch) => ch,
+            None => panic!("cannot remove a char from the end of a string"),
+        };
+        let next = idx + ch.len_utf8();
+        match self {
+            StringInner::Small { data, len } => {
+                data.copy_within(next..*len as usize, idx);
+                *len -= ch.len_utf8() as u8;
+            }
+            StringInner::Large { data, len } => {
+                *len -= ch.len_utf8();
+                match Arc::get_mut(data) {
+                    Some(old_data) => {
+                        old_data.drain(idx..next);
+                    }
+                    None => {
+                        let mut new_data = Vec::with_capacity(*len);
+                        new_data.extend_from_slice(&data.as_slice()[..idx]);
+                        new_data.extend_from_slice(&data.as_slice()[next..]);
+                        *data = Arc::from(new_data);
+                    }
+                }
+            }
+        };
+        ch
     }
 }
